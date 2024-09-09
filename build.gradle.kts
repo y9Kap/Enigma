834213fad06fa25093704b6389bdfb13754d95ac
y9Kap <wotblitz6allima4ok@gmail.com>
2024-09-10 02:18:04 +0300

    implemented forward fun on Rotor

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,42 +2,57 @@ package org.example
 
 class Rotor(private val wiring: String, private val notch: Char) {
     private var position: Int = 0
+    private val alphabetList = ('A'..'Z').toList()
 
-    fun forward(inputChar: Char): Char {
-        val inputIndex = (inputChar.uppercaseChar() - 'A' + position) % 26
-        println((inputChar.uppercaseChar() - 'A' + position) % 26)
-        println(inputIndex)
-        val wiredChar = wiring[inputIndex]
-        return wiredChar
+    fun forward(inputIndex: Int): Int {
+        val shiftedListIn = shiftList(alphabetList, position)
+
+        val firstShiftedChar = shiftedListIn[inputIndex]
+        val wiringIndex = alphabetList.indexOf(firstShiftedChar)
+        val encryptedChar = wiring[wiringIndex]
+
+        return shiftedListIn.indexOf(encryptedChar)
     }
-
-    fun backward(inputChar: Char): Char {
-        val wiredIndex = wiring.indexOf(inputChar.uppercaseChar())
-        val outputIndex = (wiredIndex - position + 26) % 26
-        return 'A' + outputIndex
-    }
-
 
     fun rotate(): Boolean {
         position = (position + 1) % 26
-        return position == (notch - 'A')
+        return position == (notch - 'A' + 1)
     }
 }
+
+fun <T> shiftList(list: List<T>, shift: Int): List<T> {
+    val size = list.size
+    if (size == 0) return list
+    val actualShift = (shift % size + size) % size
+    return list.drop(actualShift) + list.take(actualShift)
+}
+
+fun encryptThroughRotors(inputCharIndex: Int, vararg rotors: Rotor): Int {
+    return rotors.fold(inputCharIndex) { index, rotor ->
+         rotor.forward(index)
+    }
+}
+
 
 fun main() {
-
-    val rotor = Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", 'Q')
+    val rotorI = Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", 'Q')
+    val rotorII = Rotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", 'E')
+    val rotorIII = Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", 'V')
 
     while (true) {
-        val inputChar = readln().toCharArray().first()
+
+        val inputChar = readln().uppercase().first()
+        val inputIndex = inputChar - 'A'
         println("Входной символ: $inputChar")
 
-        val encryptedChar = rotor.forward(inputChar)
+        val encryptedChar = 'A' + encryptThroughRotors(inputIndex, rotorI, rotorII, rotorIII)
         println("Зашифрованный символ: $encryptedChar")
 
-        val decryptedChar = rotor.backward(encryptedChar)
-        println("Расшифрованный символ: $decryptedChar")
-        rotor.rotate()
+        if (rotorI.rotate()) {
+            if (rotorII.rotate()) {
+                rotorIII.rotate()
+            }
+        }
+
     }
 }
-
