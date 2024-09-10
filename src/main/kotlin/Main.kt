eb6c3ecf57c1654902d878fce8360069b27efbf0
y9Kap <wotblitz6allima4ok@gmail.com>
2024-09-10 14:00:04 +0300

    Emulated Enigma operation except plugboard

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,45 @@
 package org.example
 
-class Rotor(private val wiring: String, private val notch: Char) {
-    private var position: Int = 0
+class Rotor(private val wiring: String, private val notch: Char, initPosition: Char = 'A') {
+    private var position = initPosition - 'A'
     private val alphabetList = ('A'..'Z').toList()
 
     fun forward(inputIndex: Int): Int {
-        val shiftedListIn = shiftList(alphabetList, position)
+        val shiftedList = shiftList(alphabetList, position)
 
-        val firstShiftedChar = shiftedListIn[inputIndex]
-        val wiringIndex = alphabetList.indexOf(firstShiftedChar)
-        val encryptedChar = wiring[wiringIndex]
+        val firstShiftedChar = shiftedList[inputIndex]
+        val alphabetCharIndex = alphabetList.indexOf(firstShiftedChar)
+        val wiringChar = wiring[alphabetCharIndex]
 
-        return shiftedListIn.indexOf(encryptedChar)
+        return shiftedList.indexOf(wiringChar)
+    }
+
+    fun backward(inputIndex: Int): Int {
+        val shiftedList = shiftList(alphabetList, position)
+
+        val firstShiftedChar = shiftedList[inputIndex]
+        val wiringCharIndex = wiring.indexOf(firstShiftedChar)
+        val alphabetChar = alphabetList[wiringCharIndex]
+
+        return shiftedList.indexOf(alphabetChar)
     }
 
     fun rotate(): Boolean {
         position = (position + 1) % 26
-        return position == (notch - 'A' + 1)
+        return position == notch - 'A' + 1
+    }
+}
+
+class Reflector(private val wiringPairList: List<Pair<Char, Char>>) {
+    private val alphabetList = ('A'..'Z').toList()
+    fun substitution(inputIndex: Int): Int {
+        val inputChar = alphabetList[inputIndex]
+        val substitutedChar = wiringPairList.first {
+            it.first == inputChar || it.second == inputChar
+        }.let {
+            if (it.first == inputChar) it.second else it.first
+        }
+        return alphabetList.indexOf(substitutedChar)
     }
 }
 
@@ -27,26 +50,42 @@ fun <T> shiftList(list: List<T>, shift: Int): List<T> {
     return list.drop(actualShift) + list.take(actualShift)
 }
 
+fun String.toPair(): Pair<Char, Char> {
+    require(this.length == 2) { "Строка должна состоять из двух символов" }
+    return this[0] to this[1]
+}
+
 fun encryptThroughRotors(inputCharIndex: Int, vararg rotors: Rotor): Int {
     return rotors.fold(inputCharIndex) { index, rotor ->
          rotor.forward(index)
     }
 }
 
+fun decryptThroughRotors(inputCharIndex: Int, vararg rotors: Rotor): Int {
+    return rotors.fold(inputCharIndex) { index, rotor ->
+        rotor.backward(index)
+    }
+}
 
 fun main() {
     val rotorI = Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", 'Q')
     val rotorII = Rotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", 'E')
     val rotorIII = Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", 'V')
+    val reflectorWiringList = listOf(
+        "BR","CU","DH","EQ","FS","GL","IP","JX","KN","MO","TZ","VW","AY"
+    ).map { it.toPair() }
+
+    val reflector = Reflector(reflectorWiringList)
+
+    val outputCharList = mutableListOf<Char>()
+    val inputCharList = mutableListOf<Char>()
+
 
     while (true) {
 
         val inputChar = readln().uppercase().first()
+        inputCharList.add(inputChar)
         val inputIndex = inputChar - 'A'
-        println("Входной символ: $inputChar")
-
-        val encryptedChar = 'A' + encryptThroughRotors(inputIndex, rotorI, rotorII, rotorIII)
-        println("Зашифрованный символ: $encryptedChar")
 
         if (rotorI.rotate()) {
             if (rotorII.rotate()) {
@@ -54,5 +93,24 @@ fun main() {
             }
         }
 
+        val encryptedForwardRotorIndex = encryptThroughRotors(inputIndex, rotorI, rotorII, rotorIII)
+        val reflectorEncryptedCharIndex = reflector.substitution(encryptedForwardRotorIndex)
+        val res = 'A' + decryptThroughRotors(reflectorEncryptedCharIndex, rotorIII, rotorII, rotorI)
+        outputCharList.add(res)
+
+
+        if (outputCharList.size == 50) {
+            inputCharList.forEach { print(it) }
+            println("\n")
+            outputCharList.forEach { print(it) }
+        }
+
     }
 }
+//AUKADFGDAGVCDFGWERTDSAFCVBNGHTEDFGHASDFGHBVCXZZASD
+
+//FBYMCUMGILKUYKYQOGFNYYPZMPPTYLAWQJGGRWKMOSYUEBFHWM
+//--------------------------------------------------
+//FBYMCUMGILKUYKYQOGFNYYPZMPPTYLAWQJGGRWKMOSYUEBFHWM
+//
+//AUKADFGDAGVCDFGWERTDSAFCVBNGHTEDFGHASDFGHBVCXZZASD
