014da12d34265e6e8738475400ab43c309c77a69
y9Kap <wotblitz6allima4ok@gmail.com>
2024-09-10 18:25:47 +0300

    add mirroring wiring list

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -30,15 +30,11 @@ class Rotor(private val wiring: String, private val notch: Char, initPosition: C
     }
 }
 
-class Reflector(private val wiringPairList: List<Pair<Char, Char>>) {
+class Reflector(private val wiringPairList: List<String>) {
     private val alphabetList = ('A'..'Z').toList()
     fun substitution(inputIndex: Int): Int {
         val inputChar = alphabetList[inputIndex]
-        val substitutedChar = wiringPairList.first {
-            it.first == inputChar || it.second == inputChar
-        }.let {
-            if (it.first == inputChar) it.second else it.first
-        }
+        val substitutedChar = wiringPairList.toMirrorMap().getValue(inputChar)
         return alphabetList.indexOf(substitutedChar)
     }
 }
@@ -50,9 +46,18 @@ fun <T> shiftList(list: List<T>, shift: Int): List<T> {
     return list.drop(actualShift) + list.take(actualShift)
 }
 
-fun String.toPair(): Pair<Char, Char> {
-    require(this.length == 2) { "Строка должна состоять из двух символов" }
-    return this[0] to this[1]
+fun List<String>.toMirrorMap(): Map<Char, Char> {
+    val resultMap = mutableMapOf<Char, Char>()
+    for (pair in this) {
+        require(pair.length == 2) { "Each element must be a pair of characters." }
+
+        val first = pair[0]
+        val second = pair[1]
+
+        resultMap[first] = second
+        resultMap[second] = first
+    }
+    return resultMap
 }
 
 fun encryptThroughRotors(inputCharIndex: Int, vararg rotors: Rotor): Int {
@@ -73,45 +78,33 @@ fun main() {
     val rotorIII = Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", 'V')
     val reflectorWiringList = listOf(
         "BR","CU","DH","EQ","FS","GL","IP","JX","KN","MO","TZ","VW","AY"
-    ).map { it.toPair() }
+    )
 
     val reflector = Reflector(reflectorWiringList)
 
 
-
-    val outputCharList = mutableListOf<Char>()
-    val inputCharList = mutableListOf<Char>()
-
     while (true) {
+        val outputCharList = mutableListOf<Char>()
+        val inputCharList = mutableListOf<Char>()
 
-        val inputChar = readln().uppercase().first()
-        inputCharList.add(inputChar)
-        val inputIndex = inputChar - 'A'
+        val inputChar = readlnOrNull()?.uppercase()?.trim()?.toCharArray() ?: continue
+        inputChar.forEach {
+            inputCharList.add(it)
+            val inputIndex = it - 'A'
 
-        if (rotorI.rotate()) {
-            if (rotorII.rotate()) {
-                rotorIII.rotate()
+            if (rotorI.rotate()) {
+                if (rotorII.rotate()) {
+                    rotorIII.rotate()
+                }
             }
+
+            val encryptedForwardRotorIndex = encryptThroughRotors(inputIndex, rotorI, rotorII, rotorIII)
+            val reflectorEncryptedCharIndex = reflector.substitution(encryptedForwardRotorIndex)
+            val res = 'A' + decryptThroughRotors(reflectorEncryptedCharIndex, rotorIII, rotorII, rotorI)
+            outputCharList.add(res)
         }
-
-        val encryptedForwardRotorIndex = encryptThroughRotors(inputIndex, rotorI, rotorII, rotorIII)
-        val reflectorEncryptedCharIndex = reflector.substitution(encryptedForwardRotorIndex)
-        val res = 'A' + decryptThroughRotors(reflectorEncryptedCharIndex, rotorIII, rotorII, rotorI)
-        outputCharList.add(res)
-
-
-        if (outputCharList.size == 50) {
-            inputCharList.forEach { print(it) }
-            println("\n")
-            outputCharList.forEach { print(it) }
-        }
-
+        print("Вывод: ")
+        outputCharList.forEach { print(it) }
+        println()
     }
 }
-//AUKADFGDAGVCDFGWERTDSAFCVBNGHTEDFGHASDFGHBVCXZZASD
-
-//FBYMCUMGILKUYKYQOGFNYYPZMPPTYLAWQJGGRWKMOSYUEBFHWM
-//--------------------------------------------------
-//FBYMCUMGILKUYKYQOGFNYYPZMPPTYLAWQJGGRWKMOSYUEBFHWM
-//
-//AUKADFGDAGVCDFGWERTDSAFCVBNGHTEDFGHASDFGHBVCXZZASD
